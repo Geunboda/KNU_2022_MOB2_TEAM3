708512b1f23a29fd7ad3eabe005362536260292b
Geunboda <kimbo3212@gmail.com>
2022-05-10 00:48:47 +0900

    Initial Commit

---
+++ SmartScheduler/SmartSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartSchedulerApp.swift
+//  SmartScheduler
+//
+//  Created by Bogeun Kim on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct SmartSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

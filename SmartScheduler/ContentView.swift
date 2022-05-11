f0562d18c352ccd418f11f2aa3e87d318e209590
Geunboda <kimbo3212@gmail.com>
2022-05-11 22:50:18 +0900

    초기 메인 화면_비율 조정

--- SmartScheduler/ContentView.swift
+++ SmartScheduler/ContentView.swift
@@ -14,12 +14,18 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            SettingBar(content: "기본 준비 시간")
-            Spacer()
-            SettingBar(content: "기본 수면 시간")
-            Spacer()
-            SettingBar(content: "temp")
-            Button("설정 완료") {
+            ScrollView {
+                
+                Text("기본 설정").font(.title3)
+                Spacer()
+                SettingBar(content: "준비 소요 시간").offset(x: 0, y: 30)
+                Spacer()
+                SettingBar(content: "수면 시간").offset(x:0, y: 50)
+                Spacer()
+                LocationBar(content: "위치").offset(x:0, y: 70)
+                Button("설정 완료") {
+
+                }.offset(x:0, y: 100)
                 
             }
         }
@@ -35,21 +41,44 @@ struct SettingBar: View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
             shape.fill().foregroundColor(.white)
-            shape.stroke(lineWidth: 5)
+            shape.stroke(lineWidth: 2)
             Text(content)
-                .offset(x:0, y: -70)
+                .offset(x:0, y: -40)
             VStack{
                 TextField("시간을 입력하시오", text:$time)
+                    .frame(width: 200, height: 10)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .multilineTextAlignment(.center)
                     .keyboardType(.decimalPad)
-                    //.offset(x: 0, y: 70)
                     .padding()
                 Button("입력") {
                     hideKeyboard()
-                }//.offset(x:0, y: 60)
-                
-            }
+                }
+            }.offset(x:0, y: 10)
         }
+        .frame(width: 320, height: 130)
+        
+    }
+}
+
+struct LocationBar: View {
+    
+    @State var time = ""
+    var content: String
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            shape.fill().foregroundColor(.white)
+            shape.stroke(lineWidth: 2)
+            Text(content)
+                .offset(x:0, y: -100)
+            VStack{
+                
+                Button("입력") {
+                    hideKeyboard()
+                }
+            }
+        }.frame(width: 320, height: 260)
     }
 }
 #if canImport(UIKit)
@@ -60,16 +89,7 @@ extension View {
 }
 #endif
 
-struct FirstView: View {
 
-    var body: some View {
-        NavigationView{
-            List{
-                Text("Hello")
-            }
-        }
-    }
-}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
